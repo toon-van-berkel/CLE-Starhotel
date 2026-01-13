4d2cb9c5b679edcb70821ef8c11c8c5b025c5d8d
S.P.G. Wolters <stijn.wolters@ziggo.nl>
2026-01-13 15:22:52 +0100

    Nav en footer created

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<nav>
+    <ul>
+        <div class="flex">
+            <li><a class="bg-primary :hover" href="/home">home</a></li>
+            
+        </div>
+        <div class="flex">
+            <li><a href="/rooms">Rooms</a></li>
+            <li><a href="/about">About</a></li>
+        </div>
+    </ul>
+
+    <hr>
+</nav>
+
