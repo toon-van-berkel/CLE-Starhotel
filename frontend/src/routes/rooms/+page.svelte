8c60d8315064e69d075f568353c7e8d480f4b99f
Casper <caspervgameren@outlook.com>
2026-01-21 13:17:19 +0100

    UPDATED room page structure

--- frontend/src/routes/rooms/+page.svelte
+++ frontend/src/routes/rooms/+page.svelte
@@ -5,7 +5,44 @@
     const { roomsData } = data;
 </script>
 
-{#each roomsData.records as room}
+<!-- {#each roomsData.records as room}
     <div>Room {room.number} <a href="/rooms/room-{room.id}">Room {room.id}</a> </div>
+{/each} -->
+
+<div class="rooms-container">
+{#each roomsData.records as room}
+    {#if !roomsData.records}
+        <h2>Fatal error, no rooms available</h2>
+    {/if}
+    <div class="room-container">
+        <div class="popular">Popular</div>
+        <img class="room-image" src="" alt="Room {room.id} image">
+        <div class="room-body">
+            <h2>Deluxe Room</h2>
+            <article>
+                <p class=room-pricing><bold>&euro;149,99</bold> <small>Per night.</small></p>
+                <p class="room-description">Room description goes here!</p>
+            </article>
+            <div class="room-data">
+                <a href={`rooms/${room.id}`} data-sveltekit-reload>Bekijk kamer</a>
+                <p><small>Max: {room.max_capacity}</small></p>
+            </div>
+            
+            <!-- Room detail information! -->
+            <!-- <p>Max capacity: {room.max_capacity}</p>
+            <p>Current capacity{room.current_capacity}</p>
+            <p>Floor: {room.floor}</p>
+            <p>Location: {room.location}</p>
+            <p>Wing: {room.wing}</p>
+            <p>Number: {room.number}</p>
+         -->
+            <!--Might need to add room description, pricing etc-->
+        </div>
+        <!-- <p>id: {room.id}</p> -->
+        
+    </div>
+
+
 {/each}
+</div>
  
